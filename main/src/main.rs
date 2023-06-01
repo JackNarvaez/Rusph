6dc4721c49ac6ae401c97cf77e513c3027fe1097
JACKSEN JESUS NARVAEZ CORAL <jjnarvaezcor@test3460.cs.mun.ca>
2023-06-01 12:43:58 -0230

    Add parameters and constants

--- main/src/main.rs
+++ main/src/main.rs
@@ -15,6 +15,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("{}", err);
         process::exit(1);
     }
+    let t0:f64 = 0.0; // initial time
+    let tf:f64 = 1.0; // initial time
+    let dt :f64 = 0.04; // time step
+    let t_iter :u32 = ((tf-t0)/dt) as u32; // time steps
+    println!("{}", t_iter);
     let eta :f64 = 1.2; // dimensionless constant related to the ratio of smoothing length
     let sigma :f64 = 1.0/PI; // 
     let d = 3; // Dimension of the system
@@ -24,11 +29,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let r:f64 = 0.75; // Star's radius
     let nu:f64 = 1.0; // Viscocity parameter
     let lmbda = sphfunctions::coeff_static_grav_potential(k, gamma, m, r);
-    let dt :f64 = 0.001;
-    let t_iter :u32 = 10;
 
     for tt in 0..t_iter {
-        sphfunctions::smoothing_length(&mut particles, eta, sphfunctions::cubic_kernel, sphfunctions::dwdq_cubic_kernel, sigma, d, 1e-04, 100);
+        sphfunctions::smoothing_length(&mut particles, eta, sphfunctions::cubic_kernel, sphfunctions::dwdq_cubic_kernel, sigma, d, 1e-03, 100);
         sphfunctions::accelerations(&mut particles, sphfunctions::eos_polytropic, k, gamma, sphfunctions::dwdh, sphfunctions::cubic_kernel, sphfunctions::dwdq_cubic_kernel, nu, lmbda, sigma, d);
         for ii in 0..particles.len(){
             sphfunctions::euler_integrator(&mut particles[ii], dt);
