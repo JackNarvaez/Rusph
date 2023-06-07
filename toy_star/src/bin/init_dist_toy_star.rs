d7ead29967706d36b85146d80fccf42aa0ed3317
JackNarvaez <janarvaez@unal.edu.co>
2023-06-06 23:34:18 -0230

    Toy star simulation

---
+++ toy_star/src/bin/init_dist_toy_star.rs
@@ -0,0 +1,22 @@
+use std::{
+    error::Error,
+    process,
+};
+
+use sphfunctions;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = "./Data/initial_distribution/toy_star_2D.csv";
+    let n:u32 = 100; // Number of Particles
+    let m:f64 = 2.0; // Star's mass
+    let r:f64 = 0.75; // Star's radius
+    let rho:f64 = 1.0; // density
+    let dm:f64 = m/n as f64;
+    let h = 0.04 /(n as f64 /1000.).sqrt(); // Smoothing length
+    let (x0, y0) = (0.0, 0.0); // Circle's center
+    if let Err(err) = sphfunctions::init_random_circle(path, n, r, dm, rho, h, x0, y0){//sphfunctions::init_square(path, n, m, rho){
+        println!("{}", err);
+        process::exit(1);
+    }
+    Ok(())
+}
---
+++ toy_star/src/main.rs
@@ -0,0 +1,53 @@
+use std::{
+    error::Error,
+    process,
+};
+
+use sphfunctions;
+
+use std::f64::consts::PI;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path_source = "./Data/initial_distribution/toy_star_2D.csv";
+    let path_result = "./Data/results/toy_star_2D.csv";
+    let mut particles :Vec<sphfunctions::Particle> = Vec::new();
+    if let Err(err) = sphfunctions::read_data(path_source, &mut particles) {
+        println!("{}", err);
+        process::exit(1);
+    }
+    let t0:f64 = 0.0; // initial time
+    let tf:f64 = 10.0; // initial time
+    let dt :f64 = 0.004; // time step
+    let t_iter :u32 = ((tf-t0)/dt) as u32; // time steps
+    println!("{}", t_iter);
+    let eta :f64 = 1.2; // dimensionless constant related to the ratio of smoothing length
+    let d = 2; // Dimension of the system
+    let k:f64 = 0.1; // Pressure constant
+    let gamma:f64 = 1.0;  // Polytropic index
+    let m:f64 = 2.0; // Star's mass
+    let r:f64 = 0.75; // Star's radius
+    let nu:f64 = 1.0; // 1.0Viscocity parameter
+    let lmbda = sphfunctions::coeff_static_grav_potential(k, gamma, m, r);
+    let sigma :f64 = 10.0/(7.*PI); // 
+
+    for tt in 0..t_iter {
+        sphfunctions::smoothing_length(&mut particles, eta, sphfunctions::f_cubic_kernel, sphfunctions::dfdq_cubic_kernel, sigma, d, 1e-03, 100, dt);
+        sphfunctions::accelerations(&mut particles, sphfunctions::eos_polytropic, k, gamma, sphfunctions::dwdh, sphfunctions::f_cubic_kernel, sphfunctions::dfdq_cubic_kernel, sigma, d);
+        for ii in 0..particles.len(){
+            sphfunctions::body_forces_toy_star(&mut particles[ii], nu, lmbda);
+            sphfunctions::euler_integrator(&mut particles[ii], dt);
+
+            // Initialize variables to zero
+            particles[ii].ax = 0.;
+            particles[ii].ay = 0.;
+            particles[ii].dh = 0.;
+            particles[ii].du = 0.;
+        }
+        println!{"{}", tt};
+    }
+    if let Err(err) = sphfunctions::save_data(path_result, &particles){
+        println!("{}", err);
+        process::exit(1);
+    }
+    Ok(())
+}
