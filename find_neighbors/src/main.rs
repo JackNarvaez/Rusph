eed9c69a465b841e5fa743d57abfcc6e5a243a99
JACKSEN <janarvaez@unal.edu.co>
2023-06-09 12:35:10 -0230

    Build b-tree algorithm ad Test it with few uniform particles

---
+++ find_neighbors/src/main.rs
@@ -0,0 +1,40 @@
+use std::{
+    error::Error,
+    process,
+};
+
+use sphfunctions;
+use tree_algorithm::{
+    Node,
+    BuildTree,
+    save_tree,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = "./Data/tree_algorithm/set_particles.csv";
+    let path_tree = "./Data/tree_algorithm/set_tree.csv";
+    let n:u32 = 16; // Number of Particles
+    let dm:f64 = 0.1; // particle's mass
+    let w:f64 = 1.; // width
+    let l:f64 = 1.; // large
+    let rho:f64 = 1.0; // density
+    let h = 0.1; // Smoothing length
+    if let Err(err) = sphfunctions::init_square(path, n, dm, rho, h, w, l){
+        println!("{}", err);
+        process::exit(1);
+    }
+    let mut particles :Vec<sphfunctions::Particle> = Vec::new();
+    if let Err(err) = sphfunctions::read_data(path, &mut particles) {
+        println!("{}", err);
+        process::exit(1);
+    }
+    let k : u32 = 2;
+    let s : u32 = 2;
+    let alpha : f64 = 0.5;
+    let beta : f64 = 1.;
+    let mut root : Node = <Node as BuildTree>::new(n);
+    root.build_tree(k, s, alpha, beta, &particles);
+    save_tree(path_tree, &root);
+    println!("{:?}", root);
+    Ok(())
+}
--- tree_algorithm/src/lib.rs
+++ tree_algorithm/src/lib.rs
@@ -1,9 +1,14 @@
 use std::{
-    error::Error,
+    io,
     f64,
     u32,
 };
 
+use csv::Writer;
+
+use sphfunctions;
+
+#[derive(Debug)]
 pub struct Node {
 	pub xmin: f64,
     pub ymin: f64,
@@ -13,60 +18,73 @@ pub struct Node {
     pub n: u32,
     pub branches: u32,
     pub leave: bool,
-    pub children: Vec<Nodes>,
-    pub particles: Vec<u32>,
+    pub children: Vec<Node>,
+    pub particles: Vec<usize>,
 }
-trait build_tree {
+
+impl Default for Node {
+    fn default() -> Node {
+        Node {
+            xmin: -0.01,
+            ymin: -0.01,
+            side: 1.0,
+            id: 0,
+            depth: 0,
+            n: 0,
+            branches: 0,
+            leave: false,
+            children: Vec::new(),
+            particles: Vec::new(),
+        }
+    }
+}
+
+pub trait BuildTree {
+
+    fn new(n_p: u32) -> Node;
+
     fn branching_factor(& self, k: f64, s:f64) -> u32;
 
-    fn 2d_to_1d(i: u32; j :u32; b: u32) -> u32;
-    
-    fn add_particle(&self, i:f64);
-
-    fn create_child(&self, j: u32, b: u32, dx: f64) -> Box<dyn Node>;
+    fn create_child(&self, j: u32, b: u32, dx: f64) -> Node;
 
     fn create_sub_cells(&mut self, b: u32);
 
     fn delete_sub_cells(&mut self);
 
-    fn distribution_ratio(&self, limit: f64) -> f64;
+    fn delete_particles(&mut self);
 
-    fn build_tree(&mut self, k: u32, s: u32, alpha: f64, beta: f64);
+    fn distribution_ratio(&self, limit: u32) -> f64;
+
+    fn build_tree(&mut self, k: u32, s: u32, alpha: f64, beta: f64, particles: & Vec<sphfunctions::Particle>);
 }
 
-impl build_tree for Node {
+impl BuildTree for Node {
 
+    fn new(n_p: u32) -> Node {
+        Node{n: n_p, particles: (0..n_p as usize).collect(), ..Default::default()}
+    }
     fn branching_factor(& self, k: f64, s:f64) -> u32 {
-        ((self.n/s).powf(1./k)).ceil() as u32
+        ((self.n as f64 /s).powf(1./k)).ceil() as u32
     }
 
-    fn 2d_to_1d(i: u32; j :u32; b: u32) -> u32 {
-        i + j*b
-    }
-    
-    fn add_particle(&self, i:f64) {
-        self.particles.push(i);
-        self.n += 1;
-    }
-
-    fn create_child(&self, j: u32, b: u32, dx: f64) -> Box<dyn Node> {
-        return Box::new(Node{xmin: self.xmin + dx*j.div_floor(b),
-                             ymin: self.ymin + dx*(j%b),
-                             side: dx,
-                             id: j,
-                             depth: self.depth +1,
-                             n: 0,
-                             branches: 0,
-                             leave: true,
-                             children: Vec::new(),
-                             particles: Vec::new(),
-        });
+    fn create_child(&self, j: u32, b: u32, dx: f64) -> Node {
+        Node{xmin: self.xmin + dx*(j/b) as f64,
+            ymin: self.ymin + dx*(j%b) as f64,
+            side: dx,
+            id: j,
+            depth: self.depth +1,
+            n: 0,
+            branches: 0,
+            leave: true,
+            children: Vec::new(),
+            particles: Vec::new(),
+        }
     }
 
     fn create_sub_cells(&mut self, b: u32) {
         let dx = self.side / b as f64; 
         for ii in 0..self.branches {
-            self.children.push(create_child(self, ii, b, dx));
+            self.children.push(self.create_child(ii, b, dx));
         }
     }
 
@@ -78,45 +96,68 @@ impl build_tree for Node {
         self.particles = Vec::new();
     }
 
-    fn distribution_ratio(&self, limit: f64) -> f64 {
-        let r :f64 = 0.0;
-        for child in self.children{
+    fn distribution_ratio(&self, limit: u32) -> f64 {
+        let mut r :f64 = 0.0;
+        for child in &self.children{
             if child.n <= limit {
-                r += child.n;
+                r += child.n as f64;
             }
         }
         r / self.branches as f64
     }
 
-    fn build_tree(&mut self, k: u32, s: u32, alpha: f64, beta: f64) {
-        let redistribution :bool = True;
+    fn build_tree(&mut self, k: u32, s: u32, alpha: f64, beta: f64, particles: & Vec<sphfunctions::Particle>) {
+        let mut redistribution :bool = true;
         let smallest_cell = 1.0e-5;
-        let mut b = branching_factor(self, k as f64, s as f64);
+        let mut b = self.branching_factor(k as f64, s as f64);
         while redistribution {
+            println!("{}",self.depth);
             self.branches = b.pow(k);
-            create_sub_cells(self, b);
-            for p in self.particles {
-                let x_p = ((particles[p].x - self.xmin) / self.side * b as f64).floor() as u32;
-                let y_p = ((particles[p].y - self.ymin) / self.side * b as f64).floor() as u32;
-                let j = 2d_to_1d(x_p;y_p; b);
-                add_particle(self.children[j], p);
+            self.create_sub_cells(b);
+            for p in &self.particles {
+                let x_p = ((particles[*p].x - self.xmin) / self.side * b as f64).floor() as u32;
+                let y_p = ((particles[*p].y - self.ymin) / self.side * b as f64).floor() as u32;
+                let j :usize = (x_p + y_p*b).try_into().unwrap();
+                add_particle(&mut self.children[j], *p);
             }
-            let r = distribution_ratio(self, alpha * s as f64);
+            let r = self.distribution_ratio((alpha * s as f64) as u32);
             if r > beta {
+                println!("nop {}", r);
                 b /= 2;
-                delete_sub_cells(self);
+                self.delete_sub_cells();
             } else {
-                delete_particles(self);
+                self.delete_particles();
+                println!("yep {} {}", self.n, r);
                 redistribution = false;
             }
         }
-        for child in self.children {
+        for child in &mut self.children {
             if (child.n > s) && (child.side > smallest_cell) {
-                build_tree(self, k, s, alpha, beta);
+                child.build_tree(k, s, alpha, beta, particles);
             } else {
                 child.leave = true;
             }
         }
+    }
+}
 
+fn add_particle(cell: &mut Node, i: usize) {
+    cell.particles.push(i);
+    cell.n += 1;
+}
+
+pub fn save_tree(path: &str, tree: & Node){
+    let mut wtr = (Writer::from_path(path)).expect("REASON");
+    wtr.write_record(&["x_min", "y_min", "side", "depth"]);
+    save_child(&mut wtr, tree);
+}
+
+fn save_child<W: io::Write>(wtr: &mut Writer<W>, tree: & Node){
+    if tree.leave {
+        wtr.write_record(&[tree.xmin.to_string(), tree.ymin.to_string(), tree.side.to_string(), tree.depth.to_string()]);
+    } else {
+        for child in &tree.children{
+            save_child(wtr, child);
+        }
     }
 }
