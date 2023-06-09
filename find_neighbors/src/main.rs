29d23e66346c31abce4c7c5cbc3e7359eee68811
JACKSEN <janarvaez@unal.edu.co>
2023-06-09 16:41:03 -0230

    First version to built the tree: Error is not solved yet

--- find_neighbors/src/main.rs
+++ find_neighbors/src/main.rs
@@ -13,13 +13,13 @@ use tree_algorithm::{
 fn main() -> Result<(), Box<dyn Error>> {
     let path = "./Data/tree_algorithm/set_particles.csv";
     let path_tree = "./Data/tree_algorithm/set_tree.csv";
-    let n:u32 = 16; // Number of Particles
+    let n:u32 = 100; // Number of Particles
     let dm:f64 = 0.1; // particle's mass
     let w:f64 = 1.; // width
     let l:f64 = 1.; // large
     let rho:f64 = 1.0; // density
     let h = 0.1; // Smoothing length
-    if let Err(err) = sphfunctions::init_square(path, n, dm, rho, h, w, l){
+    if let Err(err) = sphfunctions::init_random_square(path, n, dm, rho, h, w, l){
         println!("{}", err);
         process::exit(1);
     }
@@ -31,7 +31,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let k : u32 = 2;
     let s : u32 = 2;
     let alpha : f64 = 0.5;
-    let beta : f64 = 1.;
+    let beta : f64 = 0.5;
     let mut root : Node = <Node as BuildTree>::new(n);
     root.build_tree(k, s, alpha, beta, &particles);
     save_tree(path_tree, &root);
--- tree_algorithm/src/lib.rs
+++ tree_algorithm/src/lib.rs
@@ -25,9 +25,9 @@ pub struct Node {
 impl Default for Node {
     fn default() -> Node {
         Node {
-            xmin: -0.01,
-            ymin: -0.01,
-            side: 1.0,
+            xmin: 0.0,
+            ymin: 0.0,
+            side: 1.,
             id: 0,
             depth: 0,
             n: 0,
@@ -68,8 +68,8 @@ impl BuildTree for Node {
     }
 
     fn create_child(&self, j: u32, b: u32, dx: f64) -> Node {
-        Node{xmin: self.xmin + dx*(j/b) as f64,
-            ymin: self.ymin + dx*(j%b) as f64,
+        Node{xmin: self.xmin + dx*(j%b) as f64,
+            ymin: self.ymin + dx*(j/b) as f64,
             side: dx,
             id: j,
             depth: self.depth +1,
@@ -82,25 +82,26 @@ impl BuildTree for Node {
     }
 
     fn create_sub_cells(&mut self, b: u32) {
-        let dx = self.side / b as f64; 
+        let dx = self.side / b as f64;
+        println!("dx  {} {}", dx, self.id);
         for ii in 0..self.branches {
             self.children.push(self.create_child(ii, b, dx));
         }
     }
 
     fn delete_sub_cells(&mut self) {
-        self.children = Vec::new();
+        self.children.clear();// = Vec::new();
     }
 
     fn delete_particles(&mut self) {
-        self.particles = Vec::new();
+        self.particles.clear();//= Vec::new();
     }
 
     fn distribution_ratio(&self, limit: u32) -> f64 {
         let mut r :f64 = 0.0;
         for child in &self.children{
             if child.n <= limit {
-                r += child.n as f64;
+                r += 1.0;
             }
         }
         r / self.branches as f64
@@ -108,26 +109,28 @@ impl BuildTree for Node {
 
     fn build_tree(&mut self, k: u32, s: u32, alpha: f64, beta: f64, particles: & Vec<sphfunctions::Particle>) {
         let mut redistribution :bool = true;
-        let smallest_cell = 1.0e-5;
+        let smallest_cell = 1.0e-2;
         let mut b = self.branching_factor(k as f64, s as f64);
         while redistribution {
-            println!("{}",self.depth);
             self.branches = b.pow(k);
+            println!("{} {} {}",self.depth, b, self.side);
             self.create_sub_cells(b);
             for p in &self.particles {
-                let x_p = ((particles[*p].x - self.xmin) / self.side * b as f64).floor() as u32;
-                let y_p = ((particles[*p].y - self.ymin) / self.side * b as f64).floor() as u32;
-                let j :usize = (x_p + y_p*b).try_into().unwrap();
+                let x_p = ((particles[*p].x - self.xmin) / self.side * b as f64).floor();
+                let y_p = ((particles[*p].y - self.ymin) / self.side * b as f64).floor();
+                let j :usize = (x_p + y_p*b as f64) as usize;
                 add_particle(&mut self.children[j], *p);
             }
             let r = self.distribution_ratio((alpha * s as f64) as u32);
-            if r > beta {
-                println!("nop {}", r);
-                b /= 2;
+            println!("{}", r);
+            if r < beta {
+                b = b/2;
+                //println!("{} {}", self.id, self.n);
                 self.delete_sub_cells();
+                //println!("Children  {}  {:?}", self.depth, self.children)
             } else {
+                //println!("yep {} {}", self.side, self.branches);
                 self.delete_particles();
-                println!("yep {} {}", self.n, r);
                 redistribution = false;
             }
         }
@@ -148,16 +151,13 @@ fn add_particle(cell: &mut Node, i: usize) {
 
 pub fn save_tree(path: &str, tree: & Node){
     let mut wtr = (Writer::from_path(path)).expect("REASON");
-    wtr.write_record(&["x_min", "y_min", "side", "depth"]);
+    wtr.write_record(&["x_min", "y_min", "side", "depth", "n"]);
     save_child(&mut wtr, tree);
 }
 
 fn save_child<W: io::Write>(wtr: &mut Writer<W>, tree: & Node){
-    if tree.leave {
-        wtr.write_record(&[tree.xmin.to_string(), tree.ymin.to_string(), tree.side.to_string(), tree.depth.to_string()]);
-    } else {
-        for child in &tree.children{
-            save_child(wtr, child);
-        }
+    wtr.write_record(&[tree.xmin.to_string(), tree.ymin.to_string(), tree.side.to_string(), tree.depth.to_string(), tree.n.to_string()]);
+    for child in &tree.children{
+        save_child(wtr, child);
     }
 }
