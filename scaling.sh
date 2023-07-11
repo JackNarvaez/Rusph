4c8e531b366d48d2fc6e0f59f0e6b562711b828c
Jacksen Narváez <88005125+JackNarvaez@users.noreply.github.com>
2023-07-11 11:08:29 -0230

    Moved to Speed up folder

--- scaling.sh
+++
@@ -1,6 +0,0 @@
-Np=20
-cargo run -p sedov_blast_wave --bin init_dist_sedov_blast_wave
-for ii in $(seq 1 ${Np}); do
-     d=$(RAYON_NUM_THREADS=${ii} cargo run -p speed_up);
-     echo ${ii} $d
-done > "./Data/speed_up/scaling.txt"
