d383f5ca999c87dc30584c1f7c536efacde220c2
Jacksen Narvaez <janarvaez@unal.edu.co>
2024-01-08 16:33:53 -0500

    Update find neighbours test with initial uniform distributions

--- tests/find_neighbors/src/main.rs
+++ tests/find_neighbors/src/main.rs
@@ -1,5 +1,4 @@
 // Tree algorithm: Tree builder and neighbors finder 
-
 use std::{
     error::Error,
     process,
@@ -8,6 +7,7 @@ use std::{
 
 use datafunctions;
 use partdistribution;
+use sphfunctions;
 
 use tree_algorithm::{
     BuildTree,
@@ -22,35 +22,41 @@ use structures::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let path = "./Data/tree_algorithm/set_particles.csv";
-    let path_tree = "./Data/tree_algorithm/set_tree.csv";
-    let path_neighbors = "./Data/tree_algorithm/set_neighbors.csv";
-    let n:u32 = 32*32*32; // Number of Particles
-    let x0:f64 = 0.; // circle's center
-    let y0:f64 = 0.; // circle's center
-    let z0:f64 = 0.; // circle's center
+    let path = "./FindNeigh/set_particles.csv";
+    let path_tree = "./FindNeigh/set_tree.csv";
+    let path_neighbors = "./FindNeigh/set_neighbors.csv";
+    let mut n:u32 = 16; // Number of Particles
+    let x0:f64 = -0.5; // circle's center
+    let y0:f64 = -0.5; // circle's center
+    let z0:f64 = -0.5; // circle's center
     let wd:f64 = 1.; // circle's center
     let lg:f64 = 1.; // circle's center
     let hg:f64 = 1.; // circle's center
-    let h :f64 = 0.1; // Smoothing length
+    let rho:f64= 1.; // Smoothing length
     let rkern :f64 = 2.; // Smoothing length
+    let eta:f64= 1.2;
 
     let xper:bool = true;
     let yper:bool = true;
     let zper:bool = true;
 
-
+    let mut particles :Vec<Particle> = Vec::new();
+    
     // Initialize system
-    if let Err(err) = partdistribution::init_square(path, n, h, wd, lg, hg, x0, y0, z0){
+    partdistribution::init_dist_hcp(&mut particles, n, n, n, rho, eta, wd, lg, hg, x0, y0, z0, 0.1);
+    if let Err(err) = datafunctions::save_data(path, &particles){
         println!("{}", err);
         process::exit(1);
     }
     // Read data
-    let mut particles :Vec<Particle> = Vec::new();
+    particles.clear();
     if let Err(err) = datafunctions::read_data(path, &mut particles) {
         println!("{}", err);
         process::exit(1);
     }
+    n = particles.len() as u32;
+    let dm: f64 = 1.0/n as f64;
+    let h: f64 = sphfunctions::h_by_density(dm, rho, eta);
 
     // Tree parameters
     let s : i32 = 4;
@@ -71,7 +77,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         root.find_neighbors(p, s, &particles, &mut neighbors, wd, lg, hg, x0, y0, z0, particles[p].h*rkern, xper, yper, zper);
         neighbors = Vec::new();
     }
-    let p: usize = 10000;
+    let p: usize = 124;
     root.find_neighbors(p, s, &particles, &mut neighbors, wd, lg, hg, x0, y0, z0, particles[p].h*rkern, xper, yper, zper);
     
     println!("Neighbors Finder: {} s", start2.elapsed().as_secs());
