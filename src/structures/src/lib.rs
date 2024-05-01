474cf5ac5ea287d72a193d2755b94b6e33e25a55
Jacksen Narvaez <janarvaez@unal.edu.co>
2024-04-30 23:08:00 -0500

    Add description of structures

--- src/structures/src/lib.rs
+++ src/structures/src/lib.rs
@@ -1,11 +1,29 @@
+// ------------------------------------------------------------------------- //
+// SPH STRUCTURES                                                            //
+// ------------------------------------------------------------------------- //
+
+
+// ------------------------------------------------------------------------- //
+// Particle: SPH particle                                                    //
+//  ptype:      u8  - 0: gas; 1: boundary; 2: dead                           //
+//  rho:        f64 - density                                                //
+//  x, y, z:    f64 - position                                               //
+//  vx, vy, vz: f64 - velocity                                               //
+//  v_xyz_star: f64 - auxiliar velocity for time integrator                  //
+//  divv:       f64 - divergence of velocity div(v)                          //
+//  ax, ay, az: f64 - acceleration                                           //
+//  u:          f64 - internal energy                                        //
+//  u_star:     f64 - auxiliar internal energy for time integrator           //
+//  du:         f64 - internal energy change                                 //
+// ------------------------------------------------------------------------- //
 #[derive(Debug)]
 pub struct Particle {
     pub ptype: u8,
+    pub rho: f64,
 	pub x: f64,
     pub y: f64,
     pub z: f64,
     pub h: f64,
-    pub rho: f64,
     pub vx: f64,
     pub vy: f64,
     pub vz: f64,
@@ -25,11 +43,11 @@ impl Default for Particle {
     fn default() -> Particle {
         Particle {
             ptype: 0,
+            rho: 1.0,
             x: 0.,
             y: 0.,
             z: 0.,
             h: 0.1,
-            rho: 1.0,
             vx: 0.0,
             vy: 0.0,
             vz: 0.0,
@@ -47,6 +65,22 @@ impl Default for Particle {
     }
 }
 
+#[derive(Copy, Clone)]
+pub struct Pointer(pub *mut Particle);
+unsafe impl Send for Pointer {}
+unsafe impl Sync for Pointer {}
+
+// ------------------------------------------------------------------------- //
+// Node: Tree node                                                           //
+//  xmin, ymin, zmin:       f64 - bottom left corner                         //
+//  sidex, sidey, sidez:    f64 - sides                                      //
+//  id:                     i32 - id                                         //
+//  depth:                  i32 - generation depth                           //
+//  n:                      i32 - number of particles                        //
+//  branches:               i32 - number of children                         //
+//  chidren:                Vec - array of children                          //
+// particles:               Vec - array of particles' id's                   //
+// ------------------------------------------------------------------------- //
 pub struct Node {
     pub xmin: f64,
     pub ymin: f64,
@@ -62,11 +96,13 @@ pub struct Node {
     pub particles: Vec<usize>,
 }
 
-#[derive(Copy, Clone)]
-pub struct Pointer(pub *mut Particle);
-unsafe impl Send for Pointer {}
-unsafe impl Sync for Pointer {}
-
+// ------------------------------------------------------------------------- //
+// Star                                                                      //
+//  m:          f64 - mass                                                   //
+//  x, y, z:    f64 - position                                               //
+//  vx, vy, vz: f64 - velocity                                               //
+//  ax, ay, az: f64 - acceleration                                           //
+// ------------------------------------------------------------------------- //
 pub struct Star {
     pub m: f64,
     pub x: f64,
